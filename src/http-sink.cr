0a20c351374385a3a8b2947dabea0789d26a715e
Caius Durling <dev@caius.name>
2022-09-10 00:07:12 +0100

    Implement basic non-loging server

--- src/http-sink.cr
+++ src/http-sink.cr
@@ -1,6 +1,25 @@
-# TODO: Write documentation for `Http::Sink`
-module Http::Sink
-  VERSION = "0.1.0"
+require "option_parser"
+require "http/server"
 
-  # TODO: Put your code here
+module HTTP::Sink
+  VERSION = "0.1.0"
 end
+
+config = {} of Symbol => String
+
+OptionParser.parse do |parser|
+  parser.banner = "HTTP Sink ready to blackhole some requests!"
+  
+  parser.on "-p PORT", "--port PORT", "Port to listen on" do |port|
+    config[:port] = port
+  end
+end
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "OK\n"
+end
+
+address = server.bind_tcp((config[:port] || "8080").to_i)
+puts "Listening on http://#{address}"
+server.listen
